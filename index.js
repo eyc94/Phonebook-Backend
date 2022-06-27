74897131630df721b6f109a30198bf578fe907ff
Eric Chin <ericychin94@gmail.com>
2022-06-26 18:38:25 -0700

    Removed hard-coded persons array and moved static middleware

--- index.js
+++ index.js
@@ -9,33 +9,10 @@ morgan.token("data", (req) => {
 });
 
 const app = express();
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
-app.use(express.static("build"));
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-];
 
 app.get("/api/persons", (request, response, next) => {
     Person.find({})
